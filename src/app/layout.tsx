ae6fd7b8ee03e79ebfc939ffaad2973f28f96f90
Lorenzo Lopez <lorenzolopez200123@gmail.com>
2024-12-03 17:24:03 -0300

    add smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" id="smooth-wrapper">
+      <body id="smooth-content">
         <VisibilityProvider>
           <Loader />
           <ThemeRegistryProvider>
--- src/providers/GsapProvider.tsx
+++ src/providers/GsapProvider.tsx
@@ -1,16 +1,39 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import gsap from 'gsap';
 
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 import { Observer } from 'gsap/Observer';
+import Lenis from 'lenis';
+
+function initialiseLenisScroll() {
+  const lenis = new Lenis({
+    smoothWheel: true,
+    duration: 1.2,
+    lerp: 0.1,
+  });
+
+  lenis.on('scroll', ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+}
 
 export const GsapProvider = ({ children }: { children: ReactNode }) => {
   if (typeof window !== 'undefined') {
-    gsap.registerPlugin(ScrollTrigger, Observer);
+    gsap.registerPlugin(ScrollTrigger, Observer, Lenis);
+    ScrollTrigger.addEventListener('refresh', () => window.scrollTo(0, 0));
+    ScrollTrigger.refresh();
   }
+
+  useEffect(() => {
+    initialiseLenisScroll();
+  }, []);
 
   return <>{children}</>;
 };
