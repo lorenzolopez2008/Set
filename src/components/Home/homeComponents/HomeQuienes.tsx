2bddd83c2e1b8089e9b0f159c8f84e726ab5fd43
Andymonzon <valentinomonzonandres@gmail.com>
2024-12-06 16:33:45 -0300

    fix clamp second section

--- src/components/Home/homeComponents/HomeQuienes.tsx
+++ src/components/Home/homeComponents/HomeQuienes.tsx
@@ -32,7 +32,7 @@ export const HomeQuienes = () => {
       {screen ? (
         <Typography
           variant={'productDescription'}
-          fontSize="clamp(0.875rem, 0.6793rem + 0.5783vw, 1.4375rem)"
+          fontSize="clamp(1.075rem, 0.6793rem + 0.5783vw, 1.4375rem)"
           marginLeft={'2rem'}
           sx={{
             textWrap: 'balance',
@@ -42,7 +42,7 @@ export const HomeQuienes = () => {
           mantenimiento y venta de equipos{' '}
           <Typography
             color="#008428"
-            fontSize="clamp(0.875rem, 0.6793rem + 0.5783vw, 1.4375rem)"
+            fontSize="clamp(1.075rem, 0.6793rem + 0.5783vw, 1.4375rem)"
             variant={'title'}
           >
             especializados para impresión de etiquetas
@@ -52,10 +52,7 @@ export const HomeQuienes = () => {
       ) : (
         <Typography
           variant={'productDescription'}
-          fontSize={{
-            lg: 'clamp(0.875rem, 0.6793rem + 0.4783vw, 1.4375rem)',
-            xl: 'clamp(0.875rem, 0.6793rem + 0.5783vw, 1.4375rem)',
-          }}
+          fontSize="clamp(0.875rem, 0.6793rem + 0.4783vw, 1.4375rem)"
           marginLeft={'2rem'}
           sx={{
             textWrap: 'balance',
@@ -65,10 +62,7 @@ export const HomeQuienes = () => {
           el mantenimiento y venta de equipos{' '}
           <Typography
             color="#008428"
-            fontSize={{
-              lg: 'clamp(0.875rem, 0.6793rem + 0.4783vw, 1.4375rem)',
-              xl: 'clamp(0.875rem, 0.6793rem + 0.5783vw, 1.4375rem)',
-            }}
+            fontSize="clamp(0.875rem, 0.6793rem + 0.4783vw, 1.4375rem)"
             variant={'title'}
           >
             especializados para <br /> impresión de etiquetas
