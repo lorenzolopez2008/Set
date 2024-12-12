ca70511493751a5716588dbd68a3393ea4e1324a
Lilnet <lololopez200123@gmail.com>
2024-12-12 17:49:02 +0000

    add spacing with footer and add title page

--- src/components/MoreProductsSection/Products.tsx
+++ src/components/MoreProductsSection/Products.tsx
@@ -2,12 +2,20 @@ import { Box } from '@mui/material';
 import { ListItems } from '@/components/MoreProductsSection/ListItems/ListItems';
 import { CardsProducts } from '@/components/MoreProductsSection/CardsProducts/CardsProducts';
 import { containerStyles } from './Products.style';
+import Title from '../Title';
 
 export const Products = () => {
   return (
-    <Box sx={containerStyles}>
-      <ListItems />
-      <CardsProducts />
+    <Box sx={{ marginBottom: '8rem' }}>
+      <Title
+        title="Nuestros Productos"
+        location="Productos"
+        titleShadow="Productos SET"
+      />
+      <Box sx={containerStyles}>
+        <ListItems />
+        <CardsProducts />
+      </Box>
     </Box>
   );
 };
